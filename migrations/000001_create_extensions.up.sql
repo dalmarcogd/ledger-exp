cf76d7d5fd91e3b4670792a8e5488eaac7d3af30
Guilherme <dalmarco.gd@gmail.com>
2022-05-11 19:03:56 -0300

    update go.mod

---
+++ migrations/000001_create_extensions.up.sql
@@ -0,0 +1,11 @@
+--
+-- Enable uuid extension
+--
+-- Use to primary keys and foreign keys in tables of issuing database
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+--
+-- Enable hstore extension
+--
+-- Use for metadata fields normally
+CREATE EXTENSION IF NOT EXISTS "hstore";
